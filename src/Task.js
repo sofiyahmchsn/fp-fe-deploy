8779a223691443b5b3a2fa1a3aa026247421a867
Sofiyah Muchsin <sofiyahmuchsin03@gmail.com>
2022-12-17 22:00:28 +0700

    siap untuk di deploy

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Sofiyah Muchsin" // TODO: replace this
+const studentId = "FE2663860" // TODO: replace this
+
+export { url, studentName, studentId }
